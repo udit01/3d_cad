9a6af10bd4d9f3e72baff76d4c84b591b63957d6
udit01 <uditjainiitd@gmail.com>
2018-03-08 20:50:25 +0530

    made the mathematical manipulation wrappers, to do same for GUI

---
+++ src/DirectionCosines.cpp
@@ -0,0 +1,43 @@
+/************************
+* Direction Cosines Class
+* Will be used in Line and Point class to perform different operations.
+*
+*************************/
+
+
+/// Class to define the direction cosines in 3d space
+class DirectionCosines
+{
+public:
+
+	/// Class might be initialized with direction cosines or ratios, to make it inclusive of both.
+	DirectionCosine(float a, float b, float c){
+		
+	}
+
+	~DirectionCosines() {
+
+	}
+
+	/// cosine of the angle made by the line/segment with the x-axis
+	float xl;
+	/// cosine of the angle made by the line/segment with the y-axis
+	float yl;
+	/// cosine of the angle made by the line/segment with the z-axis
+	float zl;
+
+
+	float dotProduct(DirectionCosine l)
+	{
+
+	}
+
+	DirectionCosine crossProduct(DirectionCosine l)
+	{
+
+	}
+
+private:
+
+};
+
---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+/// Developed by Shashank Goel & Udit Jain - AutoCad Engineering Software
+/// Main C++ file for interaction with different class files, structs and functions
+/// Constitutes the basic functionality of the Engineering Software
+
+//Include a "deepCopy()" method in all the classes, it'll help later
+#include <iostream>
+#include <math.h>
+#include <list>
+#include <glut.h>
+#include "file_interface.cpp"
+#include "gui.cpp"
+#include "shaders.cpp"
+
+using namespace std;
+
+int main(int argc, char** argv) {
+
+	return 0;
+}
